b51d8373c6aa10ee45aa6dadbcefa09f029320cd
kansal230 <87700165+kansal230@users.noreply.github.com>
2026-08-29 17:29:21 +0000

    Update backend project files

--- backend/main.py
+++ backend/main.py
@@ -2,9 +2,17 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
-    title="Choose your own aAdventure game API",
+    title="Choose your own Adventure game API",
     description="Api to generate cool stories",
     version="1.0.0",
     docs_url="/docs",
     redoc_url="/redoc",
+)
+
+app.add_middleware(
+    CORSMiddleware, # cross-origin resource sharing (CORS) middleware
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
